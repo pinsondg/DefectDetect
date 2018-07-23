93031d4795f15c007facd04bcb28dc63b12bd5a5
Vamsi Yadav <vamsiky@umich.edu>
2018-07-23 10:58:14 -0400

    Added text to view on creation

--- app/src/main/java/com/example/dpgra/defetctdetect/PotholeListAdapter.java
+++ app/src/main/java/com/example/dpgra/defetctdetect/PotholeListAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -17,20 +18,27 @@ public class PotholeListAdapter extends ArrayAdapter<Pothole> {
     private List<Pothole> list;
 
     public PotholeListAdapter(@NonNull Context context, List<Pothole> list) {
-        this.list = list;
         super(context, 0, list);
+        this.list = list;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
         Pothole pothole = getItem(position);
-
-        if ( convertView == null ) {
-            convertView = LayoutInflater.from(getContext()).inflate( R.layout.pothole, parent, false);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.pothole, parent, false);
         }
-
-
-        return super.getView(position, convertView, parent);
+        // Lookup view for data population
+        TextView coords = (TextView) convertView.findViewById(R.id.coords);
+        TextView severity = (TextView) convertView.findViewById(R.id.severity);
+        // Populate the data into the template view using the data object
+        coords.setText("Coordinates: " + pothole.getLat() + ", " + pothole.getLon());
+        severity.setText("Severity: " + pothole.getSize());
+        // Return the completed view to render on screen
+        return convertView;
     }
+
 }
