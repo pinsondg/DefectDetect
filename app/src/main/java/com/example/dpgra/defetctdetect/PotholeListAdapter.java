b042e031fd0cc8757ce9a26dad2020739ebd075d
Vamsi Yadav <vamsiky@umich.edu>
2018-07-25 10:31:59 -0400

    Search func???

--- app/src/main/java/com/example/dpgra/defetctdetect/PotholeListAdapter.java
+++ app/src/main/java/com/example/dpgra/defetctdetect/PotholeListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.dpgra.defetctdetect;
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
